a3535629ea41a567f777da47e087444d97ef624f
Koehler <ckoehler16@gmail.com>
2022-05-14 12:11:40 -0400

    finished team prompts

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const generatePage = require('./src/generatePage');
+const Employee = require('./lib/Employee');
 
 // array that holds the teams info
 const teamData = [];
 
+// start with prompts for manager info
 const promptManager = () => {
     return inquirer.prompt([
         {
@@ -50,7 +52,7 @@ const promptManager = () => {
                     return true;
                 }
                 else {
-                    console.log("Please enter the team manager's email?");
+                    console.log("Please enter the team manager's email.");
                     return false;
                 }
             }
@@ -80,6 +82,122 @@ const promptManager = () => {
         })
 };
 
+// prompts for the employees; engineers and interns
+const promptTeam = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'teamMemberRole',
+            message: 'Please select an employee to add to the team.',
+            choices: ['Add Engineer', 'Add Intern']
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the employee's name?",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                }
+                else {
+                    console.log('Please enter a name for the employee.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is the employee's Id?",
+            validate: idInput => {
+                if (idInput) {
+                    return true;
+                }
+                else {
+                    console.log("Please enter the employee's Id.");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the employee's email address?",
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                }
+                else {
+                    console.log("Please enter the employee's email.");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'githubUser',
+            message: "Please enter the engineer's GitHub username.",
+            when: (input) => input.teamMemberRole === 'Add Engineer',
+            validate: githubUserInput => {
+                if (githubUserInput) {
+                    return true;
+                }
+                else {
+                    console.log("Please enter the engineer's GitHub username.")
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'Please enter the school the intern attends.',
+            when: (input) => input.teamMemberRole === 'Add Intern',
+            validate: schoolInput => {
+                if (schoolInput) {
+                    return true;
+                }
+                else {
+                    console.log("Please enter the intern's school.")
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'addTeamMember',
+            message: 'Do you want to add another employee to the team?',
+            default: false
+        }
+    ])
+
+    // generate employee data
+    .then(({ name, id, email, teamMemberRole, githubUser, school, addTeamMember }) => {
+        let employee;
+
+        if (teamMemberRole === 'Add Engineer') {
+            employee = new Engineer (name, id, email, githubUser);
+            console.log(employee);
+        }
+
+        if (teamMemberRole === 'Add Intern') {
+            employee = new Intern (name, id, email, school);
+            console.log(employee);
+        }
+
+        // take the employee data and add it to the teamData array
+        teamData.push(employee);
+        console.log(teamData);
+
+        // run promptTeam if addTeamMember is performed
+        if (addTeamMember) {
+            return promptTeam(teamData);
+        }
+        else {
+            return teamData;
+        }
+    });
+};
+
 // call function to start prompt
-promptManager();
+promptManager()
+    .then(promptTeam);
 
